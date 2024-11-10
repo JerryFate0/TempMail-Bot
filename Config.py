dab35ba84178a8f495a05936ded69d19d9cb7298
JerryFate0 <108127048+JerryFate0@users.noreply.github.com>
2024-11-10 14:13:14 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,8 +1,8 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("7567689631:AAE57z4JSmj0RFblvJGb-mdMynV0O04s3M8")
+API_ID = int(os.environ.get("8802706"))
+API_HASH = os.environ.get("97546900397a98f481bfb8252b1ac4f4")
 
 # If You're hosting in VPS replace the following lines 9,10,11 hash symbol and replacing the approriate values on it and put the hash symbol before the lines 3,4,5
 
